b7179af9f9cc3e24904b385ad08405dac287f598
ThalesMarcel <thalesmarcels@gmail.com>
2023-11-30 14:23:52 -0300

    Resolution of exercise 06
    On branch main
    Changes to be submitted:
    	modified:   06_leapYears/leapYears.js
    	modified:   06_leapYears/leapYears.spec.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	// if 'year' is not the end of the century
+	if (year % 100) {
+		// if 'year' is not a multiple of 4
+		if (year % 4)		return false;
+		else						return true;
+	} else {
+		// if 'year' is not a multiple of 400
+		if (year % 400)	return false;
+		else						return true;
+	}
 };
 
 // Do not edit below this line
