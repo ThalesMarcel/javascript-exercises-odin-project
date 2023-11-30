7859967ac58d9fcf8111cdb30caba361f6204878
ThalesMarcel <thalesmarcels@gmail.com>
2023-11-30 10:56:04 -0300

    Resolution of exercise 04
    On branch main
    Changes to be submitted:
    	modified:   04_removeFromArray/removeFromArray.js
    	modified:   04_removeFromArray/removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+function findAllElementIndexes(array, element) {
+	const indexes = [];
+	let i = array.indexOf(element);
 
+	while (i !== -1) {
+		indexes.push(i);
+		i = array.indexOf(element, ++i);
+	}
+
+	return indexes;
+}
+
+const removeFromArray = function(array, ...elements) {
+	const result = array;
+	const indexes = new Array(elements.length);
+
+	for (let i = 0; i < elements.length; i++) {
+		indexes[i] = findAllElementIndexes(result, elements[i]);
+		for (let j = 0; j < indexes[i].length; j++)
+			result.splice(indexes[i][j], 1);
+	}
+
+	return result;
 };
 
 // Do not edit below this line
