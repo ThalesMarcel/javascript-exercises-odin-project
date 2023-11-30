a90cee9948db37faed05ea658537e32a34d92433
ThalesMarcel <thalesmarcels@gmail.com>
2023-11-30 11:42:04 -0300

    Resolution of exercise 05
    On branch main
    Changes to be submitted:
    	modified:   05_sumAll/sumAll.js
    	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	let sum = 0;
+	let intervalStart, intervalEnd;
 
+	if ((typeof(a) !== 'number') || (typeof(b) !== 'number'))	return 'ERROR';
+
+	if ((a < 0) || (b < 0))	return 'ERROR';
+
+	if (a <= b) {
+		intervalStart = a;
+		intervalEnd = b;
+	} else {
+		intervalStart = b;
+		intervalEnd = a;
+	}
+
+	for (let i = intervalStart; i <= intervalEnd; i++)	sum += i;
+
+	return sum;
 };
 
 // Do not edit below this line
